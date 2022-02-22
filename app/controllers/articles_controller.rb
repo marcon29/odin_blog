8eb2ce4f6828556671be6858e4af73f49e8ebc1b
marcon29 <online@markstabler.com>
2022-02-21 22:00:43 -0700

    Article: added edit ability, moved form to partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,23 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    # @article = Article.assign_attributes(article_params)
+    
+    if @article.update(article_params)
+    # if @article.save
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+
 
   private
     def article_params
